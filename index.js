b81dde13c812b1459b2feeb94fb203552f88998b
Keian Mar <150799485+7kei@users.noreply.github.com>
2025-03-10 16:12:04 +0800

    Setup node.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const { createServer } = require('node:http');
+const express = require('express');
+const app = express();
+const server = createServer(app);
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const { networkInterfaces } = require('os');
+
+app.use(`/`, express.static(path.join(__dirname, 'public/')));
+
+// 404 handler: keep this at the end, so it only catches requests that don’t match any defined routes
+app.use((req, res) => {
+    res.status(404);
+    res.send(`<h1>Error 404: Resource not found</h1>`);
+});
+
+// Start server
+server.listen(PORT, () => {
+    console.log("Server is running on these interfaces:");
+    const nets = networkInterfaces();
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+            if (net.family === familyV4Value && !net.internal) {
+                console.log(`${name}: http://${net.address}:${PORT}/`);
+                console.log(`${name}: http://${net.address}:${PORT}/getfeedbacks`);
+            }
+        }
+    }
+    console.log("");
+});
